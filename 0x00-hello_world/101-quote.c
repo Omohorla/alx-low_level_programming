58080c1d56c93e4878565e0a236b66a5aab5b97c
omohorla <omohorla@gmail.com>
2023-02-16 15:49:30 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - prints a string and standard error
+ *
  * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
